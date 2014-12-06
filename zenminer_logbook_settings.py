ea74ce24c25ee8713533433dff951166d48e209a
Caleb Ku <Scootie@users.noreply.github.com>
2014-12-06 02:07:53 -0600

    Create zenminer_logbook_settings.py

---
+++ zenminer_logbook_settings.py
@@ -0,0 +1,5 @@
+ZENMINER_USERNAME = 'myusername'
+ZENMINER_PASSWORD = 'mypassword'
+SPREADSHEETNAME = 'zenminer_logbook.ods'
+STOPDATE = 'update' #options are yesterday and update
+SPREADSHEET_KEY={'date': 'A', 'devicename': 'B', 'devicetype':'C', 'power':'D', 'BTCpayout':'E', 'HPpayout':'F', 'fee':'G', 'firstpool': 'I', 'firstpool_actual':'J', 'secondpool':'K', 'secondpool_actual':'H'}
